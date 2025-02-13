265b4209659eb8c2401cdd2f3df8c4cf330748db
lstrmnhc02 <johnlestermanhic18@gmail.com>
2025-02-14 03:06:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+document.addEventListener("click", function (e) {
+    let heart = document.createElement("div");
+    heart.innerHTML = "❤";
+    heart.classList.add("click-heart");
+    document.body.appendChild(heart);
+
+    let x = e.clientX;
+    let y = e.clientY;
+
+    heart.style.left = `${x}px`;
+    heart.style.top = `${y}px`;
+
+    setTimeout(() => {
+        heart.remove();
+    }, 1500);
+});
